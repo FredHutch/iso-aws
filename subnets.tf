d527c36cab5257a985cb561ade021074717be8ca
Nescot <mnescot@fredhutch.org>
2018-05-31 08:45:59 -0700

    Create separate WAF-ALB module

--- subnets.tf
+++ subnets.tf
@@ -1,12 +1,41 @@
-variable "subnet_cidr_block1" {
+variable "vpc_region" {
 type = "string"
-description = "The first block"
+description = "The VPC region"
 }
 
-variable "subnet_cidr_block2" {
+
+variable "subnet_cidr_block_pub1" {
 type = "string"
-description = "The second block"
+description = "The first public block"
 }
+
+variable "subnet_cidr_block_pub2" {
+type = "string"
+description = "The second public block"
+}
+
+variable "subnet_cidr_block_pub3" {
+type = "string"
+descripton = "The third public block"
+}
+
+
+variable "subnet_cidr_block_pri1" {
+type = "string"
+description = "The first private block"
+}
+
+variable "subnet_cidr_block_pri2" {
+type = "string"
+description = "The second private block"
+}
+
+variable = "subnet_cidr_block_pri3" {
+type = "string"
+description = "The third private block"
+}
+
+
 
 
 resource "aws_subnet" "public1" {
@@ -20,3 +49,5 @@ resource "aws_subnet" "public2" {
   availability_zone = "us-west-2b"
   cidr_block        = "${var.subnet_cidr_block2}"
 }
+
+resource "aws_subnet" pu
